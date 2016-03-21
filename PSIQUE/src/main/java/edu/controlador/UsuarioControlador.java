3ea81007f17a43baf720c9449238132ce2024be1
DavidVargasZuluaga <DavidBrootal@DavidVPc>
2016-03-21 13:40:22 -0500

    Integracion del psicologo y Autenticacion

--- PSIQUE/src/main/java/edu/controlador/UsuarioControlador.java
+++ PSIQUE/src/main/java/edu/controlador/UsuarioControlador.java
@@ -6,11 +6,9 @@ import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
@@ -25,19 +23,33 @@ import javax.servlet.http.HttpServletRequest;
 public class UsuarioControlador implements Serializable {
 
     @Inject
-    private UsuarioFacade ejbUsuario;
+    UsuarioFacade ejbUsuario;
+
+    @Inject
+    AprendizFacade aprendizFacade;
+
+    @Inject
+    PsicologoFacade psicologoFacade;
+
+    @Inject
+    CitaFacade citaFacade;
     
+
     private Usuario usuarioLog;
+    private Aprendiz aprendizLog;
     private List<Usuario> listaUsuarios;
-    
+    private Psicologo psicologoLog;
+
     @PostConstruct
-    public void init(){
+    public void init() {
         usuarioLog = new Usuario();
         listaUsuarios = new ArrayList();
+        aprendizLog = new Aprendiz();
+        psicologoLog = new Psicologo();
     }
-    
+
     public String autenticar() {
-        String res = "/index.ndvz";
+        String res = "/index.xhtml";
         FacesContext facesContext = FacesContext.getCurrentInstance();
         ExternalContext externalContext = facesContext.getExternalContext();
         Map params = externalContext.getRequestParameterMap();
@@ -47,22 +59,92 @@ public class UsuarioControlador implements Serializable {
             String clave = (String) params.get("clave");
             this.listaUsuarios = ejbUsuario.findAll();
             for (int i = 0; i < listaUsuarios.size(); i++) {
-                if (listaUsuarios.get(i).getNoDocumento() == doc && listaUsuarios.get(i).getClave().equals(clave)) {
+                if (listaUsuarios.get(i).getIdUsuario().equals(doc) && listaUsuarios.get(i).getClave().equals(clave)) {
                     this.usuarioLog = listaUsuarios.get(i);
                     httpServletRequest.getSession().setAttribute("UsuarioLog", listaUsuarios.get(i));
-                    if (listaUsuarios.get(i).getIdRol().getIdRol()==1) {
-                        res = "/Admon/admon.ndvz";
-                    } else {
-                        res = "/cuenta";
-                    }
                     break;
                 }
+            }
+            switch (usuarioLog.getIdRol().getIdRol()) {
+                case 1:
+                    res = "/index.xhtml";
+                    break;
+                case 2:
+                    res = "/index.xhtml";
+                    break;
+                case 3:
+                    psicologoLog = psicologoFacade.find(usuarioLog.getIdUsuario());
+                    res = "modPsicologo/indexPsicologo.xhtml";
+                    break;
+                case 4:
+                    aprendizLog = aprendizFacade.find(usuarioLog);
+                    res = "modAprendiz/principalAprendiz.xhtml";
+                    break;
+                default:
+                    res = "/index.xhtml";
+                    break;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         return res;
     }
-     
     
+
+    public List<Aprendiz> mostrarAprendices() {
+        List<Aprendiz> aprendices = aprendizFacade.findAll();
+        return aprendices;
+    }
+
+    public List<Cita> mostrarCitasPendientes() {
+        List<Cita> Citas = citaFacade.findAll();
+        List<Cita> citasPendientes = new ArrayList<Cita>();
+        for (int i = 0; i < Citas.size(); i++) {
+            if (Citas.get(i).getEstado().equals("CUMPLIDA")) {
+                citasPendientes.add(Citas.get(i));
+            }
+        }
+        return citasPendientes;
+    }
+
+    public List<Cita> mostrarCitasSolicitadas() {
+        List<Cita> Citas = citaFacade.findAll();
+        List<Cita> citasPendientes = new ArrayList<Cita>();
+        for (int i = 0; i < Citas.size(); i++) {
+            if (Citas.get(i).getEstado().equals("SOLICITADA")) {
+                citasPendientes.add(Citas.get(i));
+            }
+        }
+        return citasPendientes;
+    }
+
+    public List<Cita> mostrarCitas() {
+        List<Cita> Citas = citaFacade.findAll();
+        return Citas;
+    }
+
+    public Usuario getUsuarioLog() {
+        return usuarioLog;
+    }
+
+    public void setUsuarioLog(Usuario usuarioLog) {
+        this.usuarioLog = usuarioLog;
+    }
+
+    public List<Usuario> getListaUsuarios() {
+        return listaUsuarios;
+    }
+
+    public void setListaUsuarios(List<Usuario> listaUsuarios) {
+        this.listaUsuarios = listaUsuarios;
+    }
+
+    public Aprendiz getAprendizLog() {
+        return aprendizLog;
+    }
+
+    public void setAprendizLog(Aprendiz aprendizLog) {
+        this.aprendizLog = aprendizLog;
+    }
+
 }
